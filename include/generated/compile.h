1e8ab5d63ca069538261a0569289eeddf46b2c15
Joenka <geraskinp@yandex.ru>
2017-10-03 18:59:56 +0300

    custom defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Oct 3 17:55:09 MSK 2017"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Oct 3 18:56:46 MSK 2017"
 #define LINUX_COMPILE_BY "sa"
 #define LINUX_COMPILE_HOST "hpc"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.9"
+#define UTS_RELEASE "3.10.9-g752aea1-dirty"
