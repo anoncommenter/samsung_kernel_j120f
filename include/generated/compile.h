c7018a3345dd143c4bec4b4f4b6ca94c5534b007
Joenka <geraskinp@yandex.ru>
2017-10-05 13:44:13 +0300

    Deleted some trash after compiling zImage

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Oct 3 18:56:46 MSK 2017"
-#define LINUX_COMPILE_BY "sa"
-#define LINUX_COMPILE_HOST "hpc"
-#define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.10.9-g752aea1-dirty"
